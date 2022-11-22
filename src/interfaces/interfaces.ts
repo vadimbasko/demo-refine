0e41b63e095df7e9e5742c981f13db835d0455b4
Vadim Basko <vadim.p.basko@gmail.com>
2022-11-22 18:59:26 +0400

    refine's useTable hook for antd table

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,3 +1,4 @@
 export interface ICompany {
+  id: string
   name: string
 }
--- src/pages/Companies.tsx
+++ src/pages/Companies.tsx
@@ -1,23 +1,35 @@
 import React from "react";
-import {IResourceComponentsProps, MetaDataQuery, useCustom} from "@pankod/refine-core";
+import {IResourceComponentsProps, MetaDataQuery} from "@pankod/refine-core";
 import {ICompany} from "../interfaces/interfaces";
+import {Table, useTable} from "@pankod/refine-antd";
 
 export const Companies: React.FC<IResourceComponentsProps> = () => {
 
   const metaData: MetaDataQuery = {
-    operation: "companyList",
+    operation: "companyListRefine",
     fields: [
+      "id",
       "name"
     ],
+    variables: [],
   };
 
-  const { data } = useCustom<Array<ICompany>>({
-    url: '',
-    method: 'get',
+  const { tableProps } = useTable<ICompany>({
     metaData
   });
 
-  return <> {
-    data?.data?.map(company => <>{company.name}<br/></>)
-  } </>;
+  return (
+    <Table {...tableProps} rowKey="id">
+      <Table.Column
+        dataIndex="id"
+        title="ID"
+      />
+      <Table.Column
+        key="name"
+        dataIndex="name"
+        title="Name"
+      />
+    </Table>
+  )
+
 }

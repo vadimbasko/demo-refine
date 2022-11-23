e0ff1b80c74dc80e58adedd59346399fdda03a33
Vadim Basko <vadim.p.basko@gmail.com>
2022-11-23 21:47:42 +0400

    interfaces

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,4 +1,32 @@
 export interface ICompany {
   id: string
   name: string
+  staffCount?: number
+  website?: string
+}
+
+export interface IUser {
+  id: string
+  email: string
+  banned?: boolean
+  company?: ICompany
+  firstName?: string
+  lastName?: string
+  registrationDate?: Date
+  role?: IRole
+  userSource?: UserSource
+}
+
+
+export enum UserSource {
+  Mobile = "MOBILE",
+  Offline = "OFFLINE",
+  Web = "WEB",
+}
+
+export interface IRole {
+  id: string
+  name: string
+  mobileAccess?: boolean
+  webAccess?: boolean
 }
